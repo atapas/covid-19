81bd3b91f5b798d636829269f6311d193cb01916
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-03-23 16:35:20 +0530

    Headlines from json

--- src/js/Information.js
+++ src/js/Information.js
@@ -5,25 +5,17 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns';
-import Loader from 'react-loader-spinner';
 
 import covid_image from '../../assets/images/covid.png';
-
-import {useFetch} from './useFetch';
 
 import * as topHeadlines from './utils/top-headlines.json';
 
 const Information = () => {
     const API_KEY = '09037b156f3e4b27b044ef4c8fd44aaa';
-    const URL = `http://newsapi.org/v2/top-headlines?country=in&category=health&pageSize=50&apiKey=${API_KEY}`;
-    const [news, newsLoading] = useFetch(URL); 
+    const URL = `http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${API_KEY}`;
 
-    let data = [];
-    if (!newsLoading) {
-        data = news.articles;
-        console.log('data', data);
-    }
-    
+    const data = topHeadlines.articles;
+    console.log('data', data);
 
     return (
         <div className="information">
@@ -44,30 +36,20 @@ const Information = () => {
                 </Row>
             </Container>
             
-            {
-                newsLoading ? 
-                    <Loader
-                        type="ThreeDots"
-                        color="#00BFFF"
-                        height={100}
-                        width={100}
-                    /> :
-                    <CardColumns className="news">
-                        {data.map((info, index) =>
-                                <Card key={index}>
-                                    <Card.Img variant="top" src={info.urlToImage} />
-                                    <Card.Body>
-                                        <Card.Title>{info.title}</Card.Title>
-                                        <Card.Text>
-                                            {info.description}
-                                        </Card.Text>
-                                        <Card.Link target="_blank" href={info.url}>Read More..</Card.Link>
-                                    </Card.Body>
-                                </Card>
-                        )}
-                    </CardColumns>
-            }
-           
+            <CardColumns className="news">
+                {data.map((info, index) =>
+                        <Card key={index}>
+                            <Card.Img variant="top" src={info.urlToImage} />
+                            <Card.Body>
+                                <Card.Title>{info.title}</Card.Title>
+                                <Card.Text>
+                                    {info.description}
+                                </Card.Text>
+                                <Card.Link target="_blank" href={info.url}>Read More..</Card.Link>
+                            </Card.Body>
+                        </Card>
+                )}
+            </CardColumns>>
         </div>
     )
 };
