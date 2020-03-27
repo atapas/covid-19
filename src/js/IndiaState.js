bc157a2277466c3339a995efb7d73230b51494be
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-03-27 16:09:32 +0530

    Added icons in the state table

--- src/js/IndiaState.js
+++ src/js/IndiaState.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 import Card from 'react-bootstrap/Card';
 
+import red_up_arrow from '../../assets/images/up_red_arrow.png';
+import green_up_arrow from '../../assets/images/up_green_arrow.png';
+
 const IndiaState = props => {
     const stateData = props.data;
 
@@ -28,10 +31,56 @@ const IndiaState = props => {
                             {stateData.map((data) =>
                                 <tr key={data.state}>
                                     <td>{data.state}</td>
-                                    <td>{data.confirmed}</td>
-                                    <td>{data.active}</td>
-                                    <td>{data.recovered}</td>
-                                    <td>{data.deaths}</td>
+                                    <td>
+                                        <div>
+                                            <span>{data.confirmed}</span>{' '} 
+                                            {
+                                                data['delta']['active'] > 0 ?
+                                                    <span title="Increase in the Confirmed cases today">
+                                                        [<img src={red_up_arrow} height="20px" width="20px"/>
+                                                        {data['delta']['active']}]
+                                                    </span> : null
+                                            }
+                                            
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <span>{data.active}</span>{' '} 
+                                            {
+                                                data['delta']['active'] > 0 ?
+                                                    <span title="Increase in the Active cases today">
+                                                        [<img src={red_up_arrow} height="20px" width="20px"/>
+                                                        {data['delta']['active']}]
+                                                    </span> : null
+                                            }
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <span>{data.recovered}</span>{' '}
+                                            {
+                                                data['delta']['recovered'] > 0 ?
+                                                    <span title="Increase in the Recovered cases today">
+                                                        [<img src={green_up_arrow} height="20px" width="20px"/>
+                                                        {data['delta']['recovered']}]
+                                                    </span> : null
+                                            }
+                                             
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <span>{data.deaths}</span>{' '}
+                                            {
+                                                data['delta']['deaths'] > 0 ?
+                                                    <span title="Increase in the Death cases today">
+                                                        [<img src={red_up_arrow} height="20px" width="20px"/>
+                                                        {data['delta']['deaths']}]
+                                                    </span> : null
+                                            }
+                                        </div>
+                                    </td>
                                 </tr>
                             )}
                         </tbody>
