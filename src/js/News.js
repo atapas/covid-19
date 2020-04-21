5712c1b9e96f1ae7474bced42a39beacfe4d1f51
SauravKanchan <sauravnk30@gmail.com>
2020-04-22 02:19:58 +0530

    Change News API Endpoint

--- src/js/News.js
+++ src/js/News.js
@@ -13,50 +13,61 @@ import CardColumns from 'react-bootstrap/CardColumns';
 
 import covid_image from '../../assets/images/covid.png';
 
-import * as topHeadlines from './utils/top-headlines.json';
+import {useFetch} from "./useFetch";
+import Loader from "react-loader-spinner";
 
 const Information = () => {
-    const API_KEY = '09037b156f3e4b27b044ef4c8fd44aaa';
-    const URL = `http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${API_KEY}`;
+    let data = [];
+    const [topHeadlines, loadingTopHeadlines] = useFetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
 
-    const data = topHeadlines.articles;
-    console.log('data', data);
-
+    if (!loadingTopHeadlines) {
+        data = topHeadlines.articles
+    }
     return (
         <div className="information">
-            <Container className="heading">
-                <Row className="justify-content-md-center">
-                    <Col xs lg="6">
-                        <h2>
-                            <img
-                                alt={covid_image}
-                                src={covid_image}
-                                width="120"
-                                height="120"
-                                className="logo"
-                            />
-                            COVID-19 News
-                        </h2>
-                    </Col>
-                </Row>
-            </Container>
-            
-            <CardColumns className="news">
-                {data.map((info, index) =>
-                        <Card key={index}>
-                            <Card.Img variant="top" src={info.urlToImage} />
-                            <Card.Body>
-                                <Card.Title>
-                                    <Card.Link target="_blank" href={info.url}>{info.title}</Card.Link>
-                                </Card.Title>
-                                <Card.Text>
-                                    {info.description}
-                                </Card.Text>
-                                <Card.Link target="_blank" href={info.url}>Read More..</Card.Link>
-                            </Card.Body>
-                        </Card>
-                )}
-            </CardColumns>
+            {
+                loadingTopHeadlines ?
+                    <Loader
+                        type="ThreeDots"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    /> :
+                    <div>
+                        <Container className="heading">
+                            <Row className="justify-content-md-center">
+                                <Col xs lg="6">
+                                    <h2>
+                                        <img
+                                            alt={covid_image}
+                                            src={covid_image}
+                                            width="120"
+                                            height="120"
+                                            className="logo"
+                                        />
+                                        COVID-19 News
+                                    </h2>
+                                </Col>
+                            </Row>
+                        </Container>
+                        <CardColumns className="news">
+                            {data.map((info, index) =>
+                                <Card key={index}>
+                                    <Card.Img variant="top" src={info.urlToImage}/>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            <Card.Link target="_blank" href={info.url}>{info.title}</Card.Link>
+                                        </Card.Title>
+                                        <Card.Text>
+                                            {info.description}
+                                        </Card.Text>
+                                        <Card.Link target="_blank" href={info.url}>Read More..</Card.Link>
+                                    </Card.Body>
+                                </Card>
+                            )}
+                        </CardColumns>
+                    </div>
+            }
         </div>
     )
 };
